293d324a81cf5c5ee64b1ead0458498070ad25f8
Kyle Ross <6475128+kyledross@users.noreply.github.com>
2024-07-17 12:52:55 -0400

    Fixed gradient direction. Code cleanup.

--- fader.py
+++ fader.py
@@ -3,31 +3,28 @@
 # pip install adafruit-circuitpython-is31fl3741
 
 import os
-
-COLOR = "purple"
-
-os.environ["BLINKA_FT232H"] = "1"
-
 from board import I2C
 import adafruit_is31fl3741
 from adafruit_is31fl3741.adafruit_rgbmatrixqt import Adafruit_RGBMatrixQT
 from webcolors import name_to_rgb
 
-import time
+COLOR = "purple"
+
+os.environ["BLINKA_FT232H"] = "1"
 
 
-def loop_intensity(matrix):
+def loop_intensity(led_matrix):
     while True:
         # Increase intensity from 0 to 255
         for intensity in range(8, 64, 8):
-            matrix.global_current = intensity
-            matrix.show()
+            led_matrix.global_current = intensity
+            led_matrix.show()
             # time.sleep(0.01)  # Adjust the sleep time as needed
 
         # Decrease intensity from 255 to 0
         for intensity in range(64, 8, -8):
-            matrix.global_current = intensity
-            matrix.show()
+            led_matrix.global_current = intensity
+            led_matrix.show()
             # time.sleep(0.01)  # Adjust the sleep time as needed
 
 
@@ -51,17 +48,18 @@ def create_matrix() -> Adafruit_RGBMatrixQT:
 
 
 def create_face_matrix():
-    matrix = create_matrix()
+    led_matrix = create_matrix()
     # Define the coordinates for the eyes and the mouth of the smiley face
     eyes = [(5, 3), (8, 3)]
     mouth = [(4, 6), (5, 7), (6, 7), (7, 7), (8, 7), (9, 6)]
     for eye in eyes:
-        matrix.pixel(eye[0], eye[1], get_color(COLOR))
+        led_matrix.pixel(eye[0], eye[1], get_color(COLOR))
     for mouth_pixel in mouth:
-        matrix.pixel(mouth_pixel[0], mouth_pixel[1], get_color(COLOR))
-    return matrix
+        led_matrix.pixel(mouth_pixel[0], mouth_pixel[1], get_color(COLOR))
+    return led_matrix
 
-def create_gradient_matrix(matrix, start_color_int, end_color_int, width, height):
+
+def create_gradient_matrix(led_matrix, start_color_int, end_color_int, width, height):
     # Convert integer colors back to RGB components
     start_color_rgb = ((start_color_int >> 16) & 0xFF, (start_color_int >> 8) & 0xFF, start_color_int & 0xFF)
     end_color_rgb = ((end_color_int >> 16) & 0xFF, (end_color_int >> 8) & 0xFF, end_color_int & 0xFF)
@@ -76,19 +74,19 @@ def create_gradient_matrix(matrix, start_color_int, end_color_int, width, height
 
         # Apply the color to each pixel in the current column
         for y in range(height):
-            matrix.pixel(x, y, color_int)
+            led_matrix.pixel(x, y, color_int)
 
 
 # Define the start (orange) and end (purple) colors
 orange = name_to_rgb("darkorange")
-purple = name_to_rgb("blue")
+purple = name_to_rgb("purple")
 
 # Convert RGB tuples to integers
-start_color_int = create_color_int(orange.red, orange.green, orange.blue)
-end_color_int = create_color_int(purple.red, purple.green, purple.blue)
+start_color_int = create_color_int(purple.red, purple.green, purple.blue)
+end_color_int = create_color_int(orange.red, orange.green, orange.blue)
 
 # Assuming `matrix` is your Adafruit_RGBMatrixQT object
 matrix = create_matrix()
 create_gradient_matrix(matrix, start_color_int, end_color_int, 16, 9)
-matrix.global_current = 0xFF
+matrix.global_current = 64
 matrix.show()
--- test.py
+++ test.py
@@ -4,12 +4,10 @@ os.environ["BLINKA_FT232H"] = "1"
 
 # Test FT232H devices are enumerated correctly
 from pyftdi.ftdi import Ftdi
+
 Ftdi().open_from_url('ftdi:///?')
 
 # Output should be something like:
 
 # Available interfaces:
 #  ftdi://ftdi:232h:1/1  (￿￿￿￿￿￿)
-
-
-
